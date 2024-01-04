17c3de0985ad153deedd58d142dfa96d5ba9d553
Parv-gugnani <9765.parv@gmail.com>
2024-01-04 18:21:13 +0530

    sapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+// search
+//detail
 
 function App() {
+  const [state, setstate] = useState({
+    s: "Sherlock",
+    results: [],
+    selected: {},
+  });
+
+  // API
+  const apiurl = "API_KEY";
+
+  const searchInput = (e) => {
+    let s = e.target.value;
+
+    setstate((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+  //search
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        console.log(results);
+
+        setState((prevState) => {
+          return {
+            ...prevState,
+            results: results,
+          };
+        });
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">App</header>
     </div>
   );
 }
