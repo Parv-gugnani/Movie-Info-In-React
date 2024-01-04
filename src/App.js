c231133f373530296efbca1b8fab5d7b4872826a
Parv-gugnani <9765.parv@gmail.com>
2024-01-04 18:29:12 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
 // search
+import Search from "./Search";
 //detail
+import Detail from "./Detail";
 
 function App() {
-  const [state, setstate] = useState({
+  const [state, setState] = useState({
     s: "Sherlock",
     results: [],
     selected: {},
   });
 
   // API
-  const apiurl = "API_KEY";
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=98ca5c3d";
 
   const searchInput = (e) => {
     let s = e.target.value;
 
-    setstate((prevState) => {
+    setState((prevState) => {
       return { ...prevState, s: s };
     });
   };
@@ -40,10 +41,47 @@ function App() {
       });
     }
   };
+  //
+
+  const openDetail = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      setState((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
+
+  const closeDetail = () => {
+    setState((prevState) => {
+      return { ...prevState, selected: {} };
+    });
+  };
 
   return (
     <div className="App">
-      <header className="App-header">App</header>
+      <header className="App-header">
+        <h1>Movie Mania</h1>
+      </header>
+      <main>
+        <Search searchInput={searchInput} search={search} />
+
+        <div className="container">
+          {state.results.map((e) => (
+            <div className="item" onClick={() => openDetail(e.imdbID)}>
+              <img style={{ width: "200px" }} src={e.Poster} />
+              <h3 style={{ color: "white" }}>{e.Title}</h3>
+            </div>
+          ))}
+        </div>
+
+        {typeof state.selected.Title != "undefined" ? (
+          <Detail selected={state.selected} closeDetail={closeDetail} />
+        ) : (
+          false
+        )}
+      </main>
     </div>
   );
 }
